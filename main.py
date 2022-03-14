c722bbe3b2e1e4cfe374b876ff7989d4ac1cabcf
Ali İmran KAZAN <4414imran@gmail.com>
2022-03-14 22:50:41 +0300

    up

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+print ("ali imran")
+
+print ('ali imran')
+
+print ('"ali imran"')
+
+print ('ali imran\'ın')
+
+print ('ali imran \n kazan')
+
+print (r'ali imran \nabiyon lan')
+
+print (r"Ali İmran")
